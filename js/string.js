f7d5d551f9c40a72859d1398c6ebf9f4ec98c13e
Yura <y.avetisyan88@gmail.com>
2020-12-10 22:38:16 +0400

    add string js

---
+++ js/string.js
@@ -0,0 +1,13 @@
+let str = 'Some "string" for test';
+let str2 = "Some 'string' for test";
+let str3 = `Some 'string' "for test"`;
+
+let str4 = 'Som \'string\' for test';
+
+let str5 = '\tSome \n\t\tstring for \n\ttest';
+
+console.log(str);
+console.log(str2);
+console.log(str3);
+console.log(str4);
+console.log(str5);
---
+++ js/types.js
@@ -0,0 +1,33 @@
+let myString = 'some string';
+let myNumber = 10;
+let myBoolean = true;
+let myNull = null;
+let myundefinded = undefined;
+
+
+console.log('myString -- ', typeof myString);
+console.log('myNumber -- ', typeof myNumber);
+console.log('myBoolean -- ', typeof myBoolean);
+console.log('myNull -- ', typeof myNull);
+console.log('myundefinded -- ', typeof myundefinded);
+
+//parz tiper
+
+
+//obektayin tip
+
+let obj = {
+    name: 'Hi'
+};
+let arr = [1, 2, 3];
+let regexp = /w+/g;
+let func = function () {};
+
+console.log(' -------------- ');
+
+console.log(typeof obj);
+console.log(typeof arr);
+console.log(typeof regexp);
+console.log(typeof func);
+
+console.log(' -------------- ');
