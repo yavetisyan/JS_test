ea00b3433fc7bda2ecaef417cea221d289e4fc53
Yura <y.avetisyan88@gmail.com>
2020-12-16 00:08:14 +0400

    Numberss continue

---
+++ js/number.js
@@ -0,0 +1,55 @@
+// console.log(0);
+// console.log(100);
+// console.log(30000);
+
+
+// console.log('---------');
+
+// console.log(0xbcdac);
+// console.log(typeof 0Xbbbaaa);
+
+
+// console.log(Math.sqrt(25)); //5 tiv hanum a armat
+// console.log(Math.pow(5,2));//25 barcacnum e qarakusi
+// console.log(Math.PI);
+// console.log(Math.E);//exponental
+
+// let N = new Number(4000);
+
+// console.log(typeof N);
+// console.log(N);
+// console.log(N.toFixed(2));
+
+//console.log(Math.PI.toFixed(4));
+
+// let n = 5000;
+// console.log(typeof n);
+
+// console.log(n.toFixed(2));
+// console.log(n.toExponential(3));
+// console.log(n.toExponential());
+
+// console.log(n.toPrecision(10));
+
+
+//------------Operators-----------
+
+
+
+
+console.log(+10);
+console.log(-10);
+
+
+let x = 10;
+
+// console.log(++x);
+// console.log(x++);
+
+
+console.log('-------------');
+console.log(5 + 10);
+console.log(5 - 10);
+console.log(5 * 10);
+console.log(10 / 3);
+console.log(10 % 3);
