2089c578cc42bf5970dc1d0b83a485f8f4a1ae00
yavetisyan <y.avetisyan88@gmail.com>
2021-01-06 00:10:20 -0500

    Add arrays js

---
+++ js/array.js
@@ -0,0 +1,43 @@
+// let array = [1, 3, 4];
+
+
+// console.log(array);
+// array[1] = 'string';
+// console.log(typeof array[2]);
+// console.log(array);
+
+//let array = ['inchv or tox', 'aysor', 'lorem ipsum', 'mekel urish tox'];
+
+// array[20] = 'nor text';
+// console.log(array.length);
+// console.log(array.length - 1);
+// console.log(array);
+// console.log('---------');
+
+// array['someIndex'] = 'new';
+// console.log(array);
+// console.log(array.length);
+
+
+// let arr = new Array();
+// console.log(arr);
+
+
+// let text = 'hi';
+
+// console.log(Array.isArray(text));
+// console.log(Array.isArray(array));
+
+let array = ['inchv or tox', 'aysor', 'lorem ipsum', 'mekel urish tox'];
+
+// console.log(array);
+// console.log(array.join(' | '));
+// console.log('--------');
+// // console.log(array);
+// // console.log(array.reverse());
+// //console.log(array.sort()); //dasavoruma aybenakan kargi
+
+// //console.log(array.splice(1, 1, 6, 'esim inch'));
+// console.log(array);
+// delete array[3];
+// console.log(array);
